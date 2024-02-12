d57f3dd62df990259110e3b5d64caf8573c28fb3
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-12 20:07:08 +0100

    Add files via upload

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-chmod +x gradlew
+git clone https://github.com/DavidHormigoRamirez/springholamundo
+#mover el repositorio al directorio del usuario
+sudo mv ~/springholamundo /
+#abrimos el directorio
+cd /springholamundo
+#damos propiedades de ejecucion a gradlew
+sudo chmod +x gradlew
 ./gradlew war
-
-
+mv holamundo-0.0.1-SNAPSHOT-plain.war hola.war
