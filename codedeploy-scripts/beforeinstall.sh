f5c4ccc59f333290c3987d31230b78d347515c8d
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-12 20:09:53 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 git clone https://github.com/DavidHormigoRamirez/springholamundo
-#mover el repositorio al directorio del usuario
-sudo mv ~/springholamundo /
-#abrimos el directorio
-cd /springholamundo
-#damos propiedades de ejecucion a gradlew
 sudo chmod +x gradlew
 ./gradlew war
 mv holamundo-0.0.1-SNAPSHOT-plain.war hola.war
