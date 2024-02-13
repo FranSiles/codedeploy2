f485ad90712a06eb238df3d590047dd32aa8734a
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-13 13:42:08 +0100

    Add files via upload

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/d-BP5APCDG4/deployment-archive
+cd /opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 sudo chmod +x gradlew
 ./gradlew war
 cd ./build/libs
