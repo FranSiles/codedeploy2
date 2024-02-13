27a9c6fc74444fd52b5b58d2a8d99dbcc83147d5
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-13 13:09:02 +0100

    Add files via upload

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -3,4 +3,7 @@ cd /opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/d-
 sudo chmod +x gradlew
 ./gradlew war
 cd build/libs
-mv holamundo-0.0.1-SNAPSHOT-plain.war hola.war
+nombrearchivo=`ls`
+mv $nombrearchivo hola.war
+
+d-CWUA86CG4\nd-D36OW5CG4\nd-GSNYX6EG4\nd-Q0UTSNDG4
