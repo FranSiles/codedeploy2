1da3398e01ff4615cc55041cd1c640a2db559558
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-13 13:02:06 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DEPLOYMENT_ROOT="/opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/"
 DEPLOYMENT_DIR=$(ls $DEPLOYMENT_ROOT)
-cd "$DEPLOYMENT_ROOT/$DEPLOYMENT_DIR/deployment-archive"
+cd "$DEPLOYMENT_ROOT$DEPLOYMENT_DIR/deployment-archive"
 sudo chmod +x gradlew
 ./gradlew war
 cd build/libs
