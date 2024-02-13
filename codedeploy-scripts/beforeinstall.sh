2399a8177f527100d40d345fe61fc6891267504a
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-13 13:17:35 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -3,6 +3,5 @@ cd /opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/d-
 sudo chmod +x gradlew
 ./gradlew war
 cd ./build/libs
-nombrearchivo=`ls`
-mv $nombrearchivo hola.war
+mv holamundo-0.0.1-SNAPSHOT-plain.war hola.war
 
