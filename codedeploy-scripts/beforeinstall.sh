832fc235325f6214109b9591702aaaf0d3103176
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-02-13 13:19:33 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/d-Q0UTSNDG4/deployment-archive
+cd /opt/codedeploy-agent/deployment-root/ab808e4d-7f67-4f86-a554-0d4a861c908e/d-06XWNWDG4/deployment-archive
 sudo chmod +x gradlew
 ./gradlew war
 cd ./build/libs
